39f1fccc16c4443d2b06af9f514258d14f228a67
Ender <33337309+e3ndr@users.noreply.github.com>
2022-09-20 22:21:08 -0500

    Additional build stuff.

--- build.sh
+++ build.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
 TARGETARCH="arm64"
-sudo docker build --platform "linux/$TARGETARCH" --tag e3ndr/java11-node16-go1_18:$TARGETARCH ./java11-node16-go1_18
-sudo docker build --platform "linux/$TARGETARCH" --tag e3ndr/java8-node16-go1_18:$TARGETARCH ./java8-node16-go1_18
+
+sudo docker build \ 
+    --build-arg TARGETOS=linux \ 
+    --build-arg $TARGETARCH \ 
+    --tag e3ndr/java11-node16-go1_18:$TARGETARCH \ 
+    ./java11-node16-go1_18
+
+sudo docker build \ 
+    --build-arg TARGETOS=linux \ 
+    --build-arg $TARGETARCH \ 
+    --tag e3ndr/java8-node16-go1_18:$TARGETARCH \ 
+    ./java8-node16-go1_18
+
+
 
 TARGETARCH="amd64"
-sudo docker build --platform "linux/$TARGETARCH" --tag e3ndr/java11-node16-go1_18:$TARGETARCH ./java11-node16-go1_18
-sudo docker build --platform "linux/$TARGETARCH" --tag e3ndr/java8-node16-go1_18:$TARGETARCH ./java8-node16-go1_18
+
+sudo docker build \ 
+    --build-arg TARGETOS=linux \ 
+    --build-arg $TARGETARCH \ 
+    --tag e3ndr/java11-node16-go1_18:$TARGETARCH \ 
+    ./java11-node16-go1_18
+
+sudo docker build \ 
+    --build-arg TARGETOS=linux \ 
+    --build-arg $TARGETARCH \ 
+    --tag e3ndr/java8-node16-go1_18:$TARGETARCH \ 
+    ./java8-node16-go1_18
