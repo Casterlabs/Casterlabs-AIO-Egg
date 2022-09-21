9f99146234dd2f8772a36959c7be68b47f017c6c
Ender <33337309+e3ndr@users.noreply.github.com>
2022-09-20 22:25:29 -0500

    More builds things.
    Docker sucks.

--- build-arm64.sh
+++ build-arm64.sh
@@ -13,19 +13,4 @@ sudo docker build \
     --build-arg TARGETARCH=$TARGETARCH \
     --tag e3ndr/java8-node16-go1_18:$TARGETARCH \
     ./java8-node16-go1_18
-
-
-
-TARGETARCH="amd64"
-
-sudo docker build \
-    --build-arg TARGETOS=linux \
-    --build-arg TARGETARCH=$TARGETARCH \
-    --tag e3ndr/java11-node16-go1_18:$TARGETARCH \
-    ./java11-node16-go1_18
-
-sudo docker build \
-    --build-arg TARGETOS=linux \
-    --build-arg TARGETARCH=$TARGETARCH \
-    --tag e3ndr/java8-node16-go1_18:$TARGETARCH \
-    ./java8-node16-go1_18
+    
